ce5b9e43c0e57324515d91698e6688d4f7e97b97
veritasbg <veritasbg@gmail.com>
2016-05-18 15:00:12 +0300

    added

---
+++ Methods/SortingArray/SortingArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class SortingArray
+{
+    static void Main()
+    {
+        Console.ReadLine();
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        Array.Sort(array);
+
+        Console.WriteLine(string.Join(" ", array));
+    }
+}
+
