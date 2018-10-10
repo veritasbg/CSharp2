0b9e906237e32507f649408060eaf60e2a7bc4f9
veritasbg <veritasbg@gmail.com>
2018-10-10 18:57:08 +0300

    update

--- Arrays/AllocateArray/AllocateArray.cs
+++ Arrays/AllocateArray/AllocateArray.cs
@@ -19,6 +19,7 @@ namespace Test
             {
                 array[i] = 5 * i;
                 Console.WriteLine(array[i]);
+                string.Join(" ", array[i]);
             }
         }
     }
